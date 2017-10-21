70e9c9a7a863674b31f211942bc3c9ae3093d50e
Alessandro Amici <a.amici@bopen.eu>
2017-10-21 20:10:07 +0200

    Nikola auto commit.
    Nikola version: 7.8.10

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "alexamici"  # (translatable)
-BLOG_TITLE = "alexamici thoughts & ramblings - Alessandro Amici"  # (translatable)
+BLOG_TITLE = "thoughts & ramblings by Alessandro Amici / alexamici"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://alexamici.github.io/blog/"
