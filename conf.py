c9672f1a4272402cd5cd53ce4511c268d7e77e19
Alessandro Amici <a.amici@bopen.eu>
2017-10-21 19:41:13 +0200

    Nikola auto commit.
    Nikola version: 7.8.10

--- conf.py
+++ conf.py
@@ -1229,20 +1229,19 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
 # (translatable)
-# EXTRA_HEAD_DATA = ""
-# Google Analytics or whatever else you use. Added to the bottom of <body>
-# in the default template (base.tmpl).
-# (translatable)
-BODY_END = """
+EXTRA_HEAD_DATA = """
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108462437-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-108462437-1');
 </script>
 """
+# Google Analytics or whatever else you use. Added to the bottom of <body>
+# in the default template (base.tmpl).
+# (translatable)
+# BODY_END = ""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
