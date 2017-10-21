2d04139edd3d413a353bd6d53632cbf07f1e679b
Alessandro Amici <a.amici@bopen.eu>
2017-10-21 19:56:44 +0200

    Blog title.

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "alexamici"  # (translatable)
-BLOG_TITLE = "alexamici's blog"  # (translatable)
+BLOG_TITLE = "alexamici thoughts & ramblings - Alessandro Amici"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://alexamici.github.io/blog/"
