ae3deb403a92458ad2ce2c29d79c9375369d7ee6
Alessandro Amici <a.amici@bopen.eu>
2017-10-21 19:45:28 +0200

    Nikola auto commit.
    Nikola version: 7.8.10

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Alessandro Amici"  # (translatable)
+BLOG_AUTHOR = "alexamici"  # (translatable)
 BLOG_TITLE = "alexamici's blog"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
